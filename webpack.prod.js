4cb73bec25120348d55b22cf810accd50c6c43a9
rizalsuryana <rizal.suryn@gmail.com>
2025-01-06 15:52:32 +0700

    ,,

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,59 +1,22 @@
-// const {merge} = require('webpack-merge');
-// const common = require('./webpack.common');
-
-// module.exports = merge(common, {
-//   mode: 'production',
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         exclude: /node_modules/,
-//         use: [
-//           {
-//             loader: 'babel-loader',
-//             options: {
-//               presets: ['@babel/preset-env']
-//             }
-//           }
-//         ]
-//       }
-//     ]
-//   }
-// });
-
-
-// webpack.prod.js
-const { merge } = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const common = require('./webpack.common');
-const path = require('path');
 
 module.exports = merge(common, {
   mode: 'production',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
-  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',  // Ensure the correct path to your template HTML
-    }),
-  ],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        ]
+      }
+    ]
+  }
 });
